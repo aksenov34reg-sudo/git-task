b5de5ee05cbbd0142af71f709243976006e063e3
Андрей Аксенов <aksenov34reg@yandex.ru>
2026-04-23 10:14:36 +0300

    fix bugs and add sqrt

--- calc.py
+++ calc.py
@@ -1,5 +1,5 @@
-# Автор: ...
-
+# Автор: Андрей Аксенов
+import math
 def add(a, b):
     return a + b
 
@@ -9,10 +9,16 @@ def subtract(a, b):
 
 
 def multiply(a, b):
-    # TODO: не реализовано
-    return None
+    return a * b
 
+def sqrt(x):
+    if x < 0:
+        raise ValueError("Квадратный корень из отрицательного числа не определён")
+    return math.sqrt(x)
 
 if __name__ == "__main__":
     print("Простой калькулятор запущен.")
     print(f"2 + 2 = {add(2, 2)}")
+    # Тесты для новых функций:
+    print(f"3 * 4 = {multiply(3, 4)}")
+    print(f"sqrt(16) = {sqrt(16)}")
